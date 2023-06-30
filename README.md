b6f9b248a454af2c09282d30e098ab70c38a4d70
GLiegard <guillaume.liegard@ign.fr>
2023-06-30 16:08:19 +0200

    test: download test0 files at the beginning of the test

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # coclico
 
 COmparaison de CLassIfication par rapport à une référence COmmune
-
-
-# TODO
-
-# Données Test0
-
-1 LAS pour chaque dossier C1, C2 Ref. Toute petite taille 50x50.
-
--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,38 @@
 import pytest
+import os
+import requests
 
 from coclico import main
+
+remote_url = "https://github.com/IGNF/coclico-data/blob/main/"
+local_path = "./data/"
+
+files = [
+    "test0/ref/Semis_2021_0919_6424_LA93_IGN69.laz",
+    "test0/niv1/Semis_2021_0919_6424_LA93_IGN69.laz",
+    "test0/niv2/Semis_2021_0919_6424_LA93_IGN69.laz",
+    "test0/niv3/Semis_2021_0919_6424_LA93_IGN69.laz",
+    "test0/niv4/Semis_2021_0919_6424_LA93_IGN69.laz"
+]
+
+def download(remote_file, local_file):
+    response = requests.get(remote_file,verify=True, allow_redirects=True)
+    with open(local_file,'wb') as fout:
+        fout.write(response.content)
+
+
+def setup_module():
+
+    os.makedirs("./data/test0/ref/", exist_ok=True)
+    os.makedirs("./data/test0/niv1/", exist_ok=True)
+    os.makedirs("./data/test0/niv2/", exist_ok=True)
+    os.makedirs("./data/test0/niv3/", exist_ok=True)
+    os.makedirs("./data/test0/niv4/", exist_ok=True)
+
+    for file in files:
+        local_file = os.path.join(local_path, file)
+        if not os.path.exists(local_file):
+            download(os.path.join(remote_url, file), local_file)
 
 def test_compare_test0():
 
