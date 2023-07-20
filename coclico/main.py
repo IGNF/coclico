d46dafa27c1f14f4b86cb90c7d5be653903f798f
Lea Vauchier <lea.vauchier@ign.fr>
2023-07-20 14:41:45 +0200

    output: generate csv files (tilebytile and stats)

--- coclico/main.py
+++ coclico/main.py
@@ -1,18 +1,76 @@
 from pathlib import Path
 import os
 import logging
+import pandas as pd
+from typing import List
 
 
 def compare_to_ref(ci: Path, ref: Path, out: Path):
     logging.debug(f"Compare Ci: {ci} to Ref: {ref} in out {out}")
-    mpap0_out = out / "mpap0"
-    mpap0_out.mkdir(parents=True, exist_ok=True)
+    df = pd.DataFrame(
+        [
+            {"tile": "tile0", "class": 0, "metric1": 0, "metric2": 1},
+            {"tile": "tile0", "class": 1, "metric1": 2, "metric2": 3},
+            {"tile": "tile1", "class": 0, "metric1": 4, "metric2": 5},
+            {"tile": "tile1", "class": 1, "metric1": 6, "metric2": 7},
+        ]
+    )
+    df.to_csv(out, index=False)
 
-    ref_files_stem = [f.stem for f in ref.iterdir() if f.suffix.lower() in (".las", ".laz")]
 
-    for ref_file in ref_files_stem:
-        json_file = mpap0_out / f"{ref_file}.json"
-        os.system(f"touch {json_file}")
+def compute_stats(results_file: Path) -> pd.DataFrame:
+    """Compute statistics over tiles from raw result file
+
+    Args:
+        results_file (Path): path of the file containing individual tiles results
+    Returns:
+        pd.DataFrame: statistics
+    """
+    df = pd.read_csv(results_file)
+    df.drop("tile", axis=1, inplace=True)
+    df_means = df.groupby(["class"]).mean()
+    df_means.reset_index(inplace=True)  # force "class" to be a 'normal' column
+    df_means.insert(0, "statistic", "mean")  # insert statistic as first column
+    df_mins = df.groupby(["class"]).min()
+    df_mins.reset_index(inplace=True)
+    df_mins["statistic"] = "min"
+    df_maxs = df.groupby(["class"]).max()
+    df_maxs.reset_index(inplace=True)
+    df_maxs["statistic"] = "max"
+    df_medians = df.groupby(["class"]).median()
+    df_medians.reset_index(inplace=True)
+    df_medians["statistic"] = "median"
+    df_stds = df.groupby(["class"]).std()
+    df_stds.reset_index(inplace=True)
+    df_stds["statistic"] = "std"
+
+    stats_df = pd.concat([df_means, df_mins, df_maxs, df_medians, df_stds], ignore_index=True)
+    # convert dataframe with cols {stats, class, m1, m2, m3}
+    # into dataframe with cols {stats, class, metric, result}
+    ids = set(["statistic", "class"])
+    metrics = set(stats_df.columns) - ids
+    stats_df = pd.melt(stats_df, id_vars=ids, value_vars=metrics, var_name="metric", value_name="result")
+
+    return stats_df
+
+
+def merge_stats(stats_dfs: List[pd.DataFrame], out: Path):
+    """Merge statistics from several classifications
+
+    Args:
+        stats_dfs (List[pd.DataFrame]): dataframe containing statistics for a single classification
+        compared to a reference
+        out (Path): output csv file
+    """
+    # logging.debug("stats_dfs")
+    # logging.debug(stats_dfs)
+    df = stats_dfs[0]
+    df.rename(columns={"result": "result_0"}, inplace=True)
+    for ii, df_to_join in enumerate(stats_dfs[1:]):
+        df = df.merge(df_to_join, on=["statistic", "class", "metric"], validate="1:1", how="left")
+        # rename result column with index to know which is which
+        df.rename(columns={"result": f"result_{ii+1}"}, inplace=True)
+    df.to_csv(out, index=False)
 
 
 def compare(c1: Path, c2: Path, ref: Path, out: Path):
@@ -27,9 +85,17 @@ def compare(c1: Path, c2: Path, ref: Path, out: Path):
         out (Path): output path for the json outputs
     """
     logging.debug(f"Compare C1: {c1} to Ref: {ref} AND C2:{c2} to {ref} in out {out}")
+    result_by_tile_c1_file = out / "result_by_tile_c1.csv"
+    result_by_tile_c2_file = out / "result_by_tile_c2.csv"
+    result_by_metric_file = out / "result_by_metric.csv"
+    result_file = out / "result.csv"
+    out.mkdir(parents=True, exist_ok=True)
 
-    c1_out = out / "c1_ref"
-    compare_to_ref(c1, ref, c1_out)
+    compare_to_ref(c1, ref, result_by_tile_c1_file)
+    compare_to_ref(c2, ref, result_by_tile_c2_file)
+    stats_c1 = compute_stats(result_by_tile_c1_file)
+    stats_c2 = compute_stats(result_by_tile_c2_file)
 
-    c2_out = out / "c2_ref"
-    compare_to_ref(c2, ref, c2_out)
+    merge_stats([stats_c1, stats_c2], result_by_metric_file)
+
+    os.system(f"touch {result_file}")
--- test/test_main.py
+++ test/test_main.py
@@ -2,6 +2,11 @@ import os
 import requests
 from coclico import main
 from pathlib import Path
+import pandas as pd
+from typing import List, Callable
+import numpy as np
+import shutil
+
 
 remote_url = "https://github.com/IGNF/coclico-data/blob/main/"
 local_path = "./data/"
@@ -22,6 +27,9 @@ def download(remote_file, local_file):
 
 
 def setup_module():
+    tmp_path = Path("./tmp")
+    if tmp_path.is_dir():
+        shutil.rmtree(tmp_path)
     os.makedirs("./data/test0/ref/", exist_ok=True)
     os.makedirs("./data/test0/niv1/", exist_ok=True)
     os.makedirs("./data/test0/niv2/", exist_ok=True)
@@ -34,17 +42,127 @@ def setup_module():
             download(os.path.join(remote_url, file), local_file)
 
 
+def compute_toy_by_tile_result(
+    out: Path,
+    tiles: List[str] = ["tile1", "tile2"],
+    metrics: List[str] = ["metric1", "metric2"],
+    classes: List[str] = [0, 1],
+    results_fn: Callable = (lambda ii: ii),
+):
+    """Generate a toy csv file containing fake tile by tile results for provided tiles, metrics, classes
+    using results_fn to generate the result for each [tile, metric, class] from its creation index
+    This method aims to mimick coclico.main.compare_to_ref results
+
+    Args:
+        out (Path): path to the output csv file
+        tiles (List[str], optional): tiles names for which to generate results. Defaults to ["tile1", "tile2"].
+        metrics (List[str], optional): metrics for which to generate results. Defaults to ["metric1", "metric2"].
+        classes (List[str], optional): classes for which to generate results. Defaults to ["0", "1"].
+        results_fn (_type_, optional): function to use to generate values from the creation index.
+        Defaults to (lambda ii: ii).
+    """
+    results_list = []
+    ii = 0
+    for tile in tiles:
+        for cl in classes:
+            result_dict = {"tile": tile, "class": cl}
+            for m in metrics:
+                result_dict[m] = results_fn(ii)
+                ii += 1
+            results_list.append(result_dict)
+
+    df = pd.DataFrame(results_list)
+    df.to_csv(out, index=False)
+
+
+def compute_toy_stats_result(
+    stats: List[str] = ["mean", "median"],
+    metrics: List[str] = ["metric1", "metric2"],
+    classes: List[str] = [0, 1],
+    results_fn: Callable = (lambda ii: ii),
+) -> pd.DataFrame:
+    """Generate a pandas dataframe containing fake statistics results for provided stats,
+    metrics, classes using results_fn to generate the result for each [stat, metric, class]
+    This method aims to mimick coclico.main.compute_stats results
+
+    Args:
+        stats (List[str], optional): stats names for which to generate results. Defaults to ["mean", "median"].
+        metrics (List[str], optional): metric names for which to generate results. Defaults to ["metric1", "metric2"].
+        classes (List[str], optional): classes names for which to generate results. Defaults to ["0", "1"].
+        results_fn (_type_, optional): function to use to generate values from the creation index.
+        Defaults to (lambda ii: ii).
+
+    Returns:
+        pd.DataFrame: output dataframe
+    """
+    results_list = []
+    ii = 0
+    for stat in stats:
+        for cl in classes:
+            for m in metrics:
+                result_dict = {"statistic": stat, "class": cl, "metric": m, "result": results_fn(ii)}
+                ii += 1
+                results_list.append(result_dict)
+
+    df = pd.DataFrame(results_list)
+
+    return df
+
+
 def test_compare_to_ref_test0():
     c1 = Path("./data/test0/niv1/")
     ref = Path("./data/test0/ref/")
-    out = Path("./tmp/test0/compare_to_ref")
+    out = Path("./tmp/test0/compare_to_ref.csv")
+    out.parent.mkdir(parents=True, exist_ok=True)
 
     main.compare_to_ref(c1, ref, out)
 
-    ref_files_stem = [f.stem for f in ref.iterdir() if f.suffix.lower() in (".las", ".laz")]
-    for ref_file in ref_files_stem:
-        c1_ref_mpapO_json = out / "mpap0" / f"{ref_file}.json"
-        assert c1_ref_mpapO_json.is_file()
+    assert out.is_file()
+    df = pd.read_csv(out)
+    assert set(df.columns) == set(["tile", "class", "metric1", "metric2"])
+    assert len(df.index) > 0
+    assert not df.isnull().values.any()
+
+
+def test_merge_stats_toy():
+    metrics = ["m1", "m2", "m3"]
+    stats = ["mean", "std", "median", "min", "max"]
+    classes = [0, 1]
+    stats_c1_df = compute_toy_stats_result(stats, metrics, classes, (lambda ii: 2 * ii))
+    stats_c2_df = compute_toy_stats_result(stats, metrics, classes, (lambda ii: 3 * ii))
+    out = Path("./tmp/toy_results/merge_stats.csv")
+    out.parent.mkdir(parents=True, exist_ok=True)
+    main.merge_stats([stats_c1_df, stats_c2_df], out)
+    assert out.is_file()
+    df = pd.read_csv(out)
+    assert set(df.columns) == set(["statistic", "metric", "class", "result_0", "result_1"])
+    assert len(df.index) == len(metrics) * len(stats) * len(classes)
+    assert not df.isnull().values.any()
+    assert not np.all(df["result_0"] == 0)
+    assert not np.all(df["result_1"] == 0)
+    assert set(df["metric"]) == set(metrics)
+    assert set(df["statistic"]) == set(stats)
+    assert set(df["class"]) == set(classes)
+
+
+def test_compute_stats_toy():
+    metrics = ["m1", "m2", "m3"]
+    stats = ["mean", "std", "median", "min", "max"]
+    classes = [0, 1]
+    base_path = Path("./tmp/toy_results/")
+    base_path.mkdir(parents=True, exist_ok=True)
+    result_c1 = base_path / "results_by_tile_c1.csv"
+    compute_toy_by_tile_result(result_c1, ["tile1", "tile2", "tile3", "tile4"], metrics, classes, (lambda ii: 2 * ii))
+    df = main.compute_stats(result_c1)
+
+    assert set(df.columns) == set(["statistic", "metric", "class", "result"])
+    assert set(df.statistic) == set(stats)
+    assert set(df.metric) == set(metrics)
+    assert len(df.index) == len(metrics) * len(classes) * len(stats)
+    assert not df.isnull().values.any()
+
+    for s in stats:
+        assert not (np.all(df.result[df.statistic == s] == 0))
 
 
 def test_compare_test0():
@@ -55,9 +173,11 @@ def test_compare_test0():
 
     main.compare(c1, c2, ref, out)
 
-    ref_files_stem = [f.stem for f in ref.iterdir() if f.suffix.lower() in (".las", ".laz")]
-    for ref_file in ref_files_stem:
-        c1_ref_mpapO_json = out / "c1_ref" / "mpap0" / f"{ref_file}.json"
-        assert c1_ref_mpapO_json.is_file()
-        c2_ref_mpapO_json = out / "c2_ref" / "mpap0" / f"{ref_file}.json"
-        assert c2_ref_mpapO_json.is_file()
+    result_by_tile_c1_file = out / "result_by_tile_c1.csv"
+    assert (result_by_tile_c1_file).is_file()
+    result_by_tile_c2_file = out / "result_by_tile_c2.csv"
+    assert (result_by_tile_c2_file).is_file()
+    result_by_metric_file = out / "result_by_metric.csv"
+    assert (result_by_metric_file).is_file()
+    result_file = out / "result.csv"
+    assert (result_file).is_file()
