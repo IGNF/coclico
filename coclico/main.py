7f054d7aa486aadc53f068fcc109e09d5fc91f56
Lea Vauchier <lea.vauchier@ign.fr>
2023-08-09 15:08:37 +0200

    move csv manipulation outside main

--- coclico/main.py
+++ coclico/main.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import logging
 import pandas as pd
-from typing import List, Dict
+from typing import Dict
 import yaml
 
 from coclico.metrics.mpap0 import compare_one_tile_mpap0
+import coclico.tools.results_merging as merge
 
 
 METRICS = {"mpap0": compare_one_tile_mpap0, "mpap0_test": compare_one_tile_mpap0}
@@ -33,112 +34,6 @@ def compare_to_ref(ci: Path, ref: Path, out: Path, metric_weights: Dict):
     merged_df.to_csv(out, index=False)
 
 
-def compute_stats(results_file: Path) -> pd.DataFrame:
-    """Compute statistics over tiles from raw result file
-
-    Args:
-        results_file (Path): path of the file containing individual tiles results
-    Returns:
-        pd.DataFrame: statistics
-    """
-    df = pd.read_csv(results_file)
-    df.drop("tile", axis=1, inplace=True)
-    df_means = df.groupby(["class"]).mean()
-    df_means.reset_index(inplace=True)  # force "class" to be a 'normal' column
-    df_means.insert(0, "statistic", "mean")  # insert statistic as first column
-    df_mins = df.groupby(["class"]).min()
-    df_mins.reset_index(inplace=True)
-    df_mins["statistic"] = "min"
-    df_maxs = df.groupby(["class"]).max()
-    df_maxs.reset_index(inplace=True)
-    df_maxs["statistic"] = "max"
-    df_medians = df.groupby(["class"]).median()
-    df_medians.reset_index(inplace=True)
-    df_medians["statistic"] = "median"
-    df_stds = df.groupby(["class"]).std()
-    df_stds.reset_index(inplace=True)
-    df_stds["statistic"] = "std"
-
-    stats_df = pd.concat([df_means, df_mins, df_maxs, df_medians, df_stds], ignore_index=True)
-    # convert dataframe with cols {stats, class, m1, m2, m3}
-    # into dataframe with cols {stats, class, metric, result}
-    ids = set(["statistic", "class"])
-    metrics = set(stats_df.columns) - ids
-    stats_df = pd.melt(stats_df, id_vars=ids, value_vars=metrics, var_name="metric", value_name="result")
-
-    return stats_df
-
-
-def merge_stats(stats_dfs: List[pd.DataFrame], out: Path):
-    """Merge statistics from several classifications
-
-    Args:
-        stats_dfs (List[pd.DataFrame]): dataframe containing statistics for a single classification
-        compared to a reference
-        out (Path): output csv file
-    """
-    df = stats_dfs[0]
-    df.rename(columns={"result": "result_0"}, inplace=True)
-    for ii, df_to_join in enumerate(stats_dfs[1:]):
-        df = df.merge(df_to_join, on=["statistic", "class", "metric"], validate="1:1", how="left")
-        # rename result column with index to know which is which
-        df.rename(columns={"result": f"result_{ii+1}"}, inplace=True)
-    df.to_csv(out, index=False)
-
-
-def compute_weighted_result(stats_df: pd.DataFrame, weights: Dict) -> pd.DataFrame:
-    """Compute weighted sum of notes for all metrics using the weights stored in a dictionary like:
-        weights = {
-            "metric1": {
-                "class0": 1,
-                "class1": 2
-            },
-            "metric2": {
-                "class0": 0,
-                "class1": 3
-            }
-        }
-
-    Args:
-        stats_df (pd.DataFrame): dataframe containing statistics for a classification
-        weights (Dict): weights to apply to the different metrics to generate the aggregated result
-
-    Returns:
-        pd.Dataframe: pandas Dataframe containing the weighted value for each statistic
-    """
-
-    def compute_weighted_sum(group):
-        res = 0
-        for metric in weights.keys():
-            for cl in weights[metric].keys():
-                val = group[(group["metric"] == metric) & (group["class"] == cl)]["result"]
-                if len(val.index) != 1:
-                    raise ValueError(
-                        f"No or several values found for statistic={group['statistic'].iloc[0]}, "
-                        + f"class={cl}, metric={metric}. ({val.values})"
-                    )
-                res += weights[metric][cl] * val.values[0]
-
-        return res
-
-    weighted_results = stats_df.groupby("statistic").apply(compute_weighted_sum)
-    weighted_results.name = "result"
-    weighted_results = weighted_results.to_frame()
-    weighted_results.reset_index(inplace=True)
-
-    return weighted_results
-
-
-def merge_weighted_results(weighted_results: List[pd.Series], out: Path):
-    df = weighted_results[0]
-    df.rename(columns={"result": "result_0"}, inplace=True)
-    for ii, df_to_join in enumerate(weighted_results[1:]):
-        df = df.merge(df_to_join, on=["statistic"], validate="1:1", how="left")
-        # rename result column with index to know which is which
-        df.rename(columns={"result": f"result_{ii+1}"}, inplace=True)
-    df.to_csv(out, index=False)
-
-
 def read_metrics_weights(weights_file: str) -> Dict:
     with open(weights_file, "r") as f:
         weights = yaml.safe_load(f)
@@ -148,8 +43,6 @@ def read_metrics_weights(weights_file: str) -> Dict:
         raise ValueError(
             f"Metrics in {weights_file}: {list(weights.keys())} do not match expected metrics: {list(METRICS.keys())}"
         )
-
-    # assert set(weights.keys()) == set(metrics)
 
     return weights
 
@@ -174,12 +67,12 @@ def compare(c1: Path, c2: Path, ref: Path, out: Path, weights_file: Path = Path(
     out.mkdir(parents=True, exist_ok=True)
 
     compare_to_ref(c1, ref, result_by_tile_c1_file, metrics_weights)
-    stats_c1 = compute_stats(result_by_tile_c1_file)
-    result_c1 = compute_weighted_result(stats_c1, metrics_weights)
+    stats_c1 = merge.compute_stats(result_by_tile_c1_file)
+    result_c1 = merge.compute_weighted_result(stats_c1, metrics_weights)
 
     compare_to_ref(c2, ref, result_by_tile_c2_file, metrics_weights)
-    stats_c2 = compute_stats(result_by_tile_c2_file)
-    result_c2 = compute_weighted_result(stats_c2, metrics_weights)
+    stats_c2 = merge.compute_stats(result_by_tile_c2_file)
+    result_c2 = merge.compute_weighted_result(stats_c2, metrics_weights)
 
-    merge_stats([stats_c1, stats_c2], result_by_metric_file)
-    merge_weighted_results([result_c1, result_c2], result_file)
+    merge.merge_stats([stats_c1, stats_c2], result_by_metric_file)
+    merge.merge_weighted_results([result_c1, result_c2], result_file)
--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,5 @@
 from coclico import main
 from pathlib import Path
-import pandas as pd
-from typing import List, Callable
-import numpy as np
 import shutil
 import pytest
 
@@ -15,95 +12,6 @@ TMP_PATH = Path("./tmp/main")
 def setup_module():
     if TMP_PATH.is_dir():
         shutil.rmtree(TMP_PATH)
-
-
-def compute_toy_by_tile_result(
-    out: Path,
-    tiles: List[str] = ["tile1", "tile2"],
-    metrics: List[str] = ["metric1", "metric2"],
-    classes: List[str] = [0, 1],
-    results_fn: Callable = (lambda ii: ii),
-):
-    """Generate a toy csv file containing fake tile by tile results for provided tiles, metrics, classes
-    using results_fn to generate the result for each [tile, metric, class] from its creation index
-    This method aims to mimick coclico.main.compare_to_ref results
-
-    Args:
-        out (Path): path to the output csv file
-        tiles (List[str], optional): tiles names for which to generate results. Defaults to ["tile1", "tile2"].
-        metrics (List[str], optional): metrics for which to generate results. Defaults to ["metric1", "metric2"].
-        classes (List[str], optional): classes for which to generate results. Defaults to ["0", "1"].
-        results_fn (_type_, optional): function to use to generate values from the creation index.
-        Defaults to (lambda ii: ii).
-    """
-    results_list = []
-    ii = 0
-    for tile in tiles:
-        for cl in classes:
-            result_dict = {"tile": tile, "class": cl}
-            for m in metrics:
-                result_dict[m] = results_fn(ii)
-                ii += 1
-            results_list.append(result_dict)
-
-    df = pd.DataFrame(results_list)
-    df.to_csv(out, index=False)
-
-
-def compute_toy_stats_result(
-    stats: List[str] = ["mean", "median"],
-    metrics: List[str] = ["metric1", "metric2"],
-    classes: List[str] = [0, 1],
-    results_fn: Callable = (lambda ii: ii),
-) -> pd.DataFrame:
-    """Generate a pandas dataframe containing fake statistics results for provided stats,
-    metrics, classes using results_fn to generate the result for each [stat, metric, class]
-    This method aims to mimick coclico.main.compute_stats results
-
-    Args:
-        stats (List[str], optional): stats names for which to generate results. Defaults to ["mean", "median"].
-        metrics (List[str], optional): metric names for which to generate results. Defaults to ["metric1", "metric2"].
-        classes (List[str], optional): classes names for which to generate results. Defaults to ["0", "1"].
-        results_fn (_type_, optional): function to use to generate values from the creation index.
-        Defaults to (lambda ii: ii).
-
-    Returns:
-        pd.DataFrame: output dataframe
-    """
-    results_list = []
-    ii = 0
-    for stat in stats:
-        for cl in classes:
-            for m in metrics:
-                result_dict = {"statistic": stat, "class": cl, "metric": m, "result": results_fn(ii)}
-                ii += 1
-                results_list.append(result_dict)
-
-    df = pd.DataFrame(results_list)
-
-    return df
-
-
-def compute_toy_weighted_result(
-    stats: List[str] = ["mean", "median"],
-    results_fn: Callable = (lambda ii: ii),
-) -> pd.DataFrame:
-    """Generate a pandas series containing fake weigthed results for provided stats using results_fn
-    to generate the result for each stat
-    This method aims to mimick coclico.main.compute_weighted_result results
-
-    Args:
-        stats (List[str], optional): stats names for which to generate results. Defaults to ["mean", "median"].
-        results_fn (_type_, optional): function to use to generate values from the creation index.
-        Defaults to (lambda ii: ii).
-
-    Returns:
-        pd.DataFrame: output dataframe
-    """
-    result_list = [{"statistic": s, "result": results_fn(ii)} for ii, s in enumerate(stats)]
-    df = pd.DataFrame(result_list)
-
-    return df
 
 
 def test_compare_to_ref_test1(ensure_test1_data):
@@ -120,73 +28,6 @@ def test_compare_to_ref_test1(ensure_test1_data):
     assert set(df.columns) == set(["tile", "class"] + [k for k in metrics_weights.keys()])
     assert set(df["tile"]) == set(tiles)
     assert len(df.index) == nb_classes * len(tiles)
-
-
-def test_merge_stats_toy():
-    metrics = ["m1", "m2", "m3"]
-    stats = ["mean", "std", "median", "min", "max"]
-    classes = [0, 1]
-    stats_c1_df = compute_toy_stats_result(stats, metrics, classes, (lambda ii: 2 * ii))
-    stats_c2_df = compute_toy_stats_result(stats, metrics, classes, (lambda ii: 3 * ii))
-    out = TMP_PATH / Path("toy_results/merge_stats.csv")
-    out.parent.mkdir(parents=True, exist_ok=True)
-    main.merge_stats([stats_c1_df, stats_c2_df], out)
-    df = check_df_exists_with_no_empty_data(out)
-    assert set(df.columns) == set(["statistic", "metric", "class", "result_0", "result_1"])
-    assert len(df.index) == len(metrics) * len(stats) * len(classes)
-    assert not np.all(df["result_0"] == 0)
-    assert not np.all(df["result_1"] == 0)
-    assert set(df["metric"]) == set(metrics)
-    assert set(df["statistic"]) == set(stats)
-    assert set(df["class"]) == set(classes)
-
-
-def test_compute_weighted_result_toy():
-    weights = {"m1": {0: 1, 1: 0}, "m2": {0: 2}, "m3": {0: 3, 1: 3}}
-    metrics = ["m1", "m2", "m3"]
-    stats = ["mean", "std", "median", "min", "max"]
-    classes = [0, 1]
-    stats_df = compute_toy_stats_result(stats, metrics, classes, (lambda ii: 2 * ii))
-    result_df = main.compute_weighted_result(stats_df, weights)
-    assert set(result_df.columns) == set(["statistic", "result"])
-    assert set(result_df["statistic"]) == set(stats)
-    assert not result_df.isnull().values.any()
-
-
-def test_merge_weighted_results_toy():
-    out = TMP_PATH / Path("toy_results/merge_weighted_results.csv")
-    out.parent.mkdir(parents=True, exist_ok=True)
-
-    stats = ["median", "mean", "std"]
-    res_0 = compute_toy_weighted_result(stats, lambda ii: 2 * ii)
-    res_1 = compute_toy_weighted_result(stats, lambda ii: 3 * ii)
-
-    main.merge_weighted_results([res_0, res_1], out)
-    df = check_df_exists_with_no_empty_data(out)
-    assert set(df.columns) == set(["statistic", "result_0", "result_1"])
-    assert set(df["statistic"]) == set(stats)
-    assert not np.all(df["result_0"] == 0)
-    assert not np.all(df["result_1"] == 0)
-
-
-def test_compute_stats_toy():
-    metrics = ["m1", "m2", "m3"]
-    stats = ["mean", "std", "median", "min", "max"]
-    classes = [0, 1]
-    base_path = TMP_PATH / Path("toy_results/")
-    base_path.mkdir(parents=True, exist_ok=True)
-    result_c1 = base_path / "results_by_tile_c1.csv"
-    compute_toy_by_tile_result(result_c1, ["tile1", "tile2", "tile3", "tile4"], metrics, classes, (lambda ii: 2 * ii))
-    df = main.compute_stats(result_c1)
-
-    assert set(df.columns) == set(["statistic", "metric", "class", "result"])
-    assert set(df.statistic) == set(stats)
-    assert set(df.metric) == set(metrics)
-    assert len(df.index) == len(metrics) * len(classes) * len(stats)
-    assert not df.isnull().values.any()
-
-    for s in stats:
-        assert not (np.all(df.result[df.statistic == s] == 0))
 
 
 def test_compare_test1_default(ensure_test1_data):
