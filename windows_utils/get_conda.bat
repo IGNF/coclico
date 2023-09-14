03b661758ce19b183b67b118065a6ddc3f4f76a2
GLiegard <guillaume.liegard@ign.fr>
2023-09-14 16:42:30 +0200

    clean windows script

--- windows_utils/get_conda.bat
+++
@@ -1,6 +0,0 @@
-if exist %userprofile%\Anaconda3\Scripts\activate.bat call %userprofile%\Anaconda3\Scripts\activate.bat
-if exist %userprofile%\Miniconda3\Scripts\activate.bat call %userprofile%\Miniconda3\Scripts\activate.bat
-if exist  %ALLUSERSPROFILE%\Anaconda3\Scripts\activate.bat call  %ALLUSERSPROFILE%\Anaconda3\Scripts\activate.bat
-if exist %ALLUSERSPROFILE%\Miniconda3\Scripts\activate.bat call %ALLUSERSPROFILE%\Miniconda3\Scripts\activate.bat
-if exist %userprofile%\mambaforge\Scripts\activate.bat call %userprofile%\mambaforge\Scripts\activate.bat
-if exist %ALLUSERSPROFILE%\mambaforge\Scripts\activate.bat call %ALLUSERSPROFILE%\mambaforge\Scripts\activate.bat
