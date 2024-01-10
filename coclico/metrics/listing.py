b9f5e7139547158cd6c272ef60022cac0dd657d9
Yoann Apel <yoann.apel@ign.fr>
2024-01-10 17:06:16 +0100

    mobj0: add basic intrinsic metric

--- coclico/metrics/listing.py
+++ coclico/metrics/listing.py
@@ -1,5 +1,6 @@
 from coclico.malt0.malt0 import MALT0
+from coclico.mobj0.mobj0 import MOBJ0
 from coclico.mpap0.mpap0 import MPAP0
 from coclico.mpla0.mpla0 import MPLA0
 
-METRICS = {"mpap0": MPAP0, "mpla0": MPLA0, "malt0": MALT0}
+METRICS = {"mpap0": MPAP0, "mpla0": MPLA0, "malt0": MALT0, "mobj0": MOBJ0}
---
+++ coclico/mobj0/mobj0_intrinsic.py
@@ -0,0 +1,89 @@
+import argparse
+import logging
+from pathlib import Path
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import rasterio
+from osgeo import gdal
+from rasterio.features import shapes as rasterio_shapes
+from shapely.geometry import shape as shapely_shape
+
+import coclico.io
+from coclico.metrics.occupancy_map import create_occupancy_map_array, read_las
+from coclico.mobj0.mobj0 import MOBJ0
+
+gdal.UseExceptions()
+
+
+def create_objects_array(las_file, pixel_size, class_weights):
+    xs, ys, classifs, crs = read_las(las_file)
+
+    binary_maps, x_min, y_max = create_occupancy_map_array(xs, ys, classifs, pixel_size, class_weights)
+
+    return binary_maps, crs, x_min, y_max
+
+
+def vectorize_occupancy_map(binary_maps, crs, x_min, y_max, pixel_size):
+    # Create empty dataframe
+    gdf_list = []
+
+    for ii, map_layer in enumerate(binary_maps):
+        shapes_layer = rasterio_shapes(
+            map_layer,
+            connectivity=8,
+            transform=rasterio.transform.from_origin(
+                x_min - pixel_size / 2, y_max + pixel_size / 2, pixel_size, pixel_size
+            ),
+        )
+
+        geometries = [shapely_shape(shapedict) for shapedict, value in shapes_layer if value != 0]
+        nb_geometries = len(geometries)
+        gdf_list.append(
+            gpd.GeoDataFrame(
+                {"layer": ii * np.ones(nb_geometries), "geometry": geometries},
+                geometry="geometry",
+                crs=crs,
+            )
+        )
+
+    gdf = pd.concat(gdf_list)
+
+    return gdf
+
+
+def compute_metric_intrinsic(las_file: Path, config_file: Path, output_geojson: Path, pixel_size: float = 0.5):
+    config_dict = coclico.io.read_config_file(config_file)
+    class_weights = config_dict[MOBJ0.metric_name]["weights"]
+    output_geojson.parent.mkdir(parents=True, exist_ok=True)
+    obj_array, crs, x_min, y_max = create_objects_array(las_file, pixel_size, class_weights)
+    polygons_gdf = vectorize_occupancy_map(obj_array, crs, x_min, y_max, pixel_size)
+    polygons_gdf.to_file(output_geojson)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Run malt0 intrinsic metric on one tile")
+    parser.add_argument("-i", "--input-file", type=Path, required=True, help="Path to the LAS file")
+    parser.add_argument("-o", "--output-geojson", type=Path, required=True, help="Path to the output geojson")
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        type=Path,
+        required=True,
+        help="Coclico configuration file",
+    )
+    parser.add_argument(
+        "-p", "--pixel-size", type=float, required=True, help="Pixel size of the intermediate occupancy map"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+    compute_metric_intrinsic(
+        las_file=Path(args.input_file),
+        config_file=args.config_file,
+        output_geojson=Path(args.output_geojson),
+    )
